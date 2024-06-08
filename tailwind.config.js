fb87ddc328dec64512c78ac12db9a4f1c41f1b07
Syidh13 <syidh.illuminaty@gmail.com>
2024-06-08 16:30:56 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layouts/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+};
